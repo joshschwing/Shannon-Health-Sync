b97a913564cd479d6445fa8fb994a29b1ca24b0f
joshuaschwing <jschwing@angelo.edu>
2023-09-30 16:53:51 -0500

    Initial Commit

---
+++ Shannon Health Sync/Shannon_Health_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shannon_Health_SyncApp.swift
+//  Shannon Health Sync
+//
+//  Created by Joshua Schwing on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Shannon_Health_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
